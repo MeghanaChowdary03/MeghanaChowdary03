774a8bcca0c502def966fd9a28b7ce83ee0323f7
MeghanaChowdary03 <112332286+MeghanaChowdary03@users.noreply.github.com>
2022-09-27 13:30:58 +0530

    Add files via upload

---
+++ lexicography.py
@@ -0,0 +1,11 @@
+def countrings(n,start):
+    if n==0:
+        return 1
+    cnt=0
+    for i in range(start,5):
+        cnt+=countstrings(n-1,i)
+    return cnt
+def countvowelstrings(n):
+    return countstrings(n,0)
+n=int(input("enterinput"))
+print("countvowelstrings",(n))
